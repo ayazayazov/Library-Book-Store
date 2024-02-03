226fb36b8303c2194dbb3c656806260c34be80d7
Nurlana <nurlana@cubics.technology>
2024-02-03 21:11:32 +0400

    add contact data with firebase

---
+++ src/js/contact-us.js
@@ -0,0 +1,126 @@
+
+/* ======================== HTML elements ========================= */
+let contact_form = document.getElementById("contact_form");
+let name_input = document.querySelector("#name");
+let phone_input = document.querySelector("#phone");
+let address_input = document.querySelector("#address");
+let email_input = document.querySelector("#email");
+let alert_box = document.querySelector(".alert_box")
+
+
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+    getDatabase,
+    ref,
+    push,
+    child,
+    set,
+    get,
+    onValue,
+    update,
+    remove,
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC6XY1TEOFV42mGOrBmRP0G2aD4HDTapNc",
+    authDomain: "library-book-store-125e4.firebaseapp.com",
+    databaseURL: "https://library-book-store-125e4-default-rtdb.firebaseio.com",
+    projectId: "library-book-store-125e4",
+    storageBucket: "library-book-store-125e4.appspot.com",
+    messagingSenderId: "925545310186",
+    appId: "1:925545310186:web:8d3daf513aa050ad5ab493"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const db = getDatabase()
+
+function writeSetContactData(collection, data) {
+    try {
+        if (!collection) {
+            alert('Required collection')
+            return
+        }
+        const contactRef = ref(db, collection)
+        console.log(contactRef, 'contactRef')
+        set(contactRef, data)
+    } catch (err) {
+        console.log(err, 'err')
+    }
+}
+
+function writePushContactData(collection, data) {
+    try {
+        if (!collection) {
+            alert('Required collection')
+            return
+        }
+        const contactRef = ref(db, collection)
+        push(contactRef, data)
+        // location.reload()
+    } catch (err) {
+        console.log(err, 'err')
+    }
+}
+
+contact_form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let name = e.target.name.value;
+    let email = e.target.email.value;
+    let phone = e.target.phone.value;
+    let address = e.target.address.value;
+    let note = e.target.note.value;
+
+    if (!name) {
+        name_input.classList.add("is-invalid")
+    } else {
+        name_input.classList.remove("is-invalid")
+    }
+    if (!email) {
+        email_input.classList.add("is-invalid")
+    } else {
+        email_input.classList.remove("is-invalid")
+    }
+    if (!phone) {
+        phone_input.classList.add("is-invalid")
+    } else {
+        phone_input.classList.remove("is-invalid")
+    }
+    if (!address) {
+        address_input.classList.add("is-invalid")
+    } else {
+        address_input.classList.remove("is-invalid")
+    }
+
+    if (!name || !email || !phone || !address) {
+        return
+    }
+    let form = {
+        name,
+        email,
+        phone,
+        address,
+        note
+    }
+    writePushContactData('contacts', form)
+    e.target.name.value = "";
+    e.target.email.value = "";
+    e.target.phone.value = "";
+    e.target.address.value = "";
+    e.target.note.value = "";
+
+    if (form) {
+        alert_box.innerHTML = `
+        <div class="alert mt-3 alert-success" role="alert">
+          Form added!
+        </div>
+        `
+    }
+    setTimeout(()=>{
+        alert_box.innerHTML = ""
+    },2000)
+})
